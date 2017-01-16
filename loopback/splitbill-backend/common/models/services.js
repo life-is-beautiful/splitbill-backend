4ab8b562e9f75185e990c7dbaa358feab4017cea
Michael Edvard Ranndall Tarigan <tarikgan@gmail.com>
2017-01-16 13:06:48 +0700

    playaround to creating custom endpoint

--- loopback/splitbill-backend/common/models/services.js
+++ loopback/splitbill-backend/common/models/services.js
@@ -3,9 +3,20 @@
 module.exports = function(Services) {
 
   Services.remoteMethod('splashScreen',{
+    accepts: {arg: 'deviceId', type: 'string', required: true},
     http: {path: '/splashScreen', verb: 'post'},
     returns: {arg: 'status', type: 'Object'}
   })
+
+  Services.splashScreen = function(deviceId, cb) {
+
+    //query the database for a single matching dog
+    Customer.findOne({where: {device: deviceId}}, function(err, dog) {
+
+      //return only the location property of the dog
+      cb(null, dog.location);
+    });
+  }
 
   Services.remoteMethod('registerUser',{
     http: {path: '/registerUser', verb: 'post'},

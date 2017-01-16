01a4c394745cc947b3082764e853811e83cca9cc
Michael Edvard Ranndall Tarigan <tarikgan@gmail.com>
2017-01-16 11:11:56 +0700

    add Cloudant as datasource

---
+++ loopback/splitbill-backend/common/models/services.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(Services) {
+  Services.remoteMethod('splashScreen',{
+    http: {path: '/splashScreen', verb: 'get'},
+    returns: {arg: 'status', type: 'Object'}
+  })
+
+  Services.remoteMethod('registerUser',{
+    http: {path: '/registerUser', verb: 'post'},
+    returns: {arg: 'ok', type: 'Object'}
+  })
+};
--- loopback/splitbill-backend/server/boot/customer.js
+++ loopback/splitbill-backend/server/boot/customer.js
@@ -1,6 +1,3 @@
-/**
- * Created by 8tmeran on 1/15/2017.
- */
 module.exports = function(app) {
   delete app.models.User.validations.email;
 };
